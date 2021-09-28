cd446bc5f5d048dfc1e55049321b7f9b25ad8458
Artem Serov <artemserovusa@gmail.com>
2021-09-28 19:09:32 -0400

    Updated docstring for user_stats function

--- bikeshare.py
+++ bikeshare.py
@@ -189,7 +189,7 @@ def trip_duration_stats(df):
 
 
 def user_stats(df):
-    """Displays statistics on bikeshare users."""
+    """Displays basic statistics on bikeshare users."""
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
